b8ce9c249870464b6878a627b27ebd1b19c02185
Lanxxxe <lancegula05@gmail.com>
2025-08-23 10:23:30 -0400

    add placeholders on login form add suffix on registration

--- core/views.py
+++ core/views.py
@@ -13,6 +13,7 @@ def register(request):
         first_name = request.POST.get('first_name', '').strip()
         middle_name = request.POST.get('middle_name', '').strip()
         last_name = request.POST.get('last_name', '').strip()
+        suffix = request.POST.get('suffix', '').strip()
         email = request.POST.get('email', '').strip()
         phone = request.POST.get('phone', '').strip()
         address = request.POST.get('address', '').strip()
@@ -40,6 +41,7 @@ def register(request):
                 first_name=first_name,
                 middle_name=middle_name,
                 last_name=last_name,
+                suffix=suffix,
                 email=email,
                 phone=phone,
                 address=address,
