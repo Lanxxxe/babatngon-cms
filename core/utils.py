b647412abd7e1ef93db371f04e33541f3e317b8d
Lanxxxe <lancegula05@gmail.com>
2025-10-29 13:43:01 -0400

    fix import issues and add notification page

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,7 @@
+from .models import Notification  # Import here to avoid circular imports
 def add_notification(request, context):
     """
     Adds a notification for the specified user.
     """
-    from .models import Notification  # Import here to avoid circular imports
     user = context.get('current_user')
     message = context.get('notification_message')
