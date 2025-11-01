b0f11afa27446402bc75e7a56f34c853d6398ea3
Lanxxxe <lancegula05@gmail.com>
2025-11-01 11:22:15 -0400

    add is_archived attribute on user model

--- core/models.py
+++ core/models.py
@@ -14,6 +14,7 @@ class User(models.Model):
     address = models.TextField(blank=True, null=True)
     password = models.CharField(max_length=255)
     is_verified = models.BooleanField(default=False)
+    is_archived = models.BooleanField(default=False)
     profile_picture = models.ImageField(upload_to='uploads/profile_pictures/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
